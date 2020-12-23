d36e55ef6cfeea05e1ff2533ae387f0e79a2504c
kiethwood <kiethwood@utah.gov>
2020-12-23 14:02:15 -0700

    initial commit

---
+++ src/main/java/dkw/beer/srvr/BreweryServerApplication.java
@@ -0,0 +1,13 @@
+package dkw.beer.srvr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BreweryServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BreweryServerApplication.class, args);
+	}
+
+}
